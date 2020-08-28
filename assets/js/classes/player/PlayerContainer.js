eaa7506131b6277f0b2a80dc5a9d080efef97fcf
cammac <camm@rocktechcomputer.com>
2020-08-28 11:28:59 -0700

    added weapon game object

--- assets/js/classes/player/PlayerContainer.js
+++ assets/js/classes/player/PlayerContainer.js
@@ -1,9 +1,19 @@
+const Direction = {
+  RIGHT: 'RIGHT',
+  LEFT: 'LEFT',
+  UP: 'UP',
+  DOWN: 'DOWN',
+}
 
 class PlayerContainer extends Phaser.GameObjects.Container {
   constructor(scene, x, y, key, frame) {
     super(scene, x, y);
     this.scene = scene; // the scene this contianer will be added to
     this.velocity = 160; // the velocity when moving the player
+    this.currentDirection = Direction.RIGHT;
+    this.palyerAttacking = false;
+    this.flipX = true;
+    this.swordHit = false;
 
     // set a size on the container
     this.setSize(64, 64);
@@ -19,6 +29,14 @@ class PlayerContainer extends Phaser.GameObjects.Container {
     // create the player
     this.player = new Player(this.scene, 0, 0, key, frame);
     this.add(this.player);
+
+    // create the weapon game object
+    this.weapon = this.scene.add.image(40, 0, 'items', 4);
+    this.scene.add.existing(this.weapon);
+    this.weapon.setScale(1.5);
+    this.scene.physics.world.enable(this.weapon);
+    this.add(this.weapon);
+    this.weapon.alpha = 1;
   }
 
 
