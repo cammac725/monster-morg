a3d4e94025f2285a1bdb46bda54ed036b89f8bce
cammac <camm@rocktechcomputer.com>
2020-08-31 17:10:06 -0700

    add instructions to title scene

--- assets/js/scenes/TitleScene.js
+++ assets/js/scenes/TitleScene.js
@@ -7,20 +7,33 @@ class TitleScene extends Phaser.Scene {
     // create title text
     this.titleText = this.add.text(
       this.scale.width / 2,
-      this.scale.height / 2,
+      this.scale.height / 3.5,
       'Monster Morg',
       {
         fontSize: '64px',
         fill: '#fff',
       },
     );
+
+    // create move text
+    this.instructionText = this.add.text(
+      this.scale.width / 2,
+      this.scale.height * 0.75,
+      'Find gold chests\n\nDefeat monsters\n\nMove: arrow keys\n\nAttack: space bar',
+      {
+        fontSize: '20px',
+        fill: '#fff',
+      },
+    );
+   
     this.titleText.setOrigin(0.5);
+    this.instructionText.setOrigin(0.5);
     
     // create the Play Game button
     this.startGameButton = new UiButton(
       this,
       this.scale.width / 2,
-      this.scale.height * 0.65,
+      this.scale.height * 0.5,
       'button1',
       'button2',
       'Start',
